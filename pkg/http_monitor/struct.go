09ba4e5f1138cbefffaf7c2de62e61154d1dd3bc
hongfs <hong@hongfs.cn>
2023-11-03 22:11:24 +0800

    Update struct.go

--- pkg/http_monitor/struct.go
+++ pkg/http_monitor/struct.go
@@ -4,16 +4,17 @@ type BaseRequest struct {
 	Type string `json:"type"`
 }
 
-type TaskItem struct {
-	Type 	 string `json:"type"`
+type TaskRequest struct {
+	Type     string `json:"type"`
 	UUID     string `json:"uuid"`
 	URL      string `json:"url"`
+	Timeout  uint   `json:"timeout"`
 	HTTPCode int    `json:"http_code"`
 	ErrMsg   string `json:"err_msg"`
 }
 
 type AuthRequest struct {
-	Type string `json:"type"`
+	Type  string `json:"type"`
 	Token string `json:"token"`
 }
 
