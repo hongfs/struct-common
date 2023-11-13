ade669f424ecf85a4b3389c9b4453e6ef49fe7db
hongfs <hong@hongfs.cn>
2023-11-13 13:26:09 +0800

    Update struct.go

--- pkg/http_monitor/struct.go
+++ pkg/http_monitor/struct.go
@@ -9,8 +9,15 @@ type TaskRequest struct {
 	UUID     string `json:"uuid"`
 	URL      string `json:"url"`
 	Timeout  uint   `json:"timeout"`
-	HTTPCode int    `json:"http_code"`
-	ErrMsg   string `json:"err_msg"`
+	TargetIP string `json:"target_ip,omitempty"`
+}
+
+type TaskResponse struct {
+	Type        string  `json:"type"`
+	UUID        string  `json:"uuid"`
+	HTTPCode    int     `json:"http_code,omitempty"`
+	ErrMsg      string  `json:"err_msg,omitempty"`
+	ConsumeTime float64 `json:"consume_time,omitempty"`
 }
 
 type AuthRequest struct {
